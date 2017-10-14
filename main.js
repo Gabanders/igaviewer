8c94f7cc7f257d8af1dd1dcc07636800e6518abf
risonakamo <hmykanae@gmail.com>
2017-10-13 21:54:17 -0400

    basic viewer functionality +polymer +igviewer element

--- main.js
+++ main.js
@@ -1,20 +1,35 @@
 window.onload=main;
 
+var igview;
+var imgs;
+
 function main()
 {
+    igview=document.querySelector("ig-view");
+
+    getAlbum("HhmyT",(r)=>{
+        imgs=[];
+        for (var x=0,l=r.data.length;x<l;x++)
+        {
+            imgs.push(r.data[x].link);
+        }
+
+        igview.loadImgs(imgs);
+    });
+}
+
+/*string url: imgur album tag string thing
+  function callback(object response): callback function
+    object response: result album*/
+function getAlbum(url,callback)
+{
     var r=new XMLHttpRequest();
-    r.open("GET","https://api.imgur.com/3/album/HhmyT/images");
+    r.open("GET",`https://api.imgur.com/3/album/${url}/images`);
 
     r.onreadystatechange=()=>{
         if (r.readyState==4)
         {
-            var ipoint=document.querySelector(".imgs");
-            var d=JSON.parse(r.response).data;
-
-            for (var x=0;x<d.length;x++)
-            {
-                ipoint.insertAdjacentHTML("beforeend",`<img src="${d[x].link}">`);
-            }
+            callback(JSON.parse(r.response));
         }
     };
 
